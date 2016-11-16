f12014a45a3028f98c63c33a23c898cee46f72b5
Tony Fouchard <tony.fouchard@blablacar.com>
2016-11-16 15:25:14 +0100

    Introduce dict template func

--- bin-templater/template/templating.go
+++ bin-templater/template/templating.go
@@ -3,6 +3,7 @@ package template
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/leekchan/gtf"
 	"gopkg.in/yaml.v2"
@@ -79,6 +80,21 @@ func (t *Templating) AddFunctions(fs map[string]interface{}) {
 }
 
 ///////////////////////////////////
+
+func dict(values ...interface{}) (map[string]interface{}, error) {
+	if len(values)%2 != 0 {
+		return nil, errors.New("invalid dict call")
+	}
+	dict := make(map[string]interface{}, len(values)/2)
+	for i := 0; i < len(values); i += 2 {
+		key, ok := values[i].(string)
+		if !ok {
+			return nil, errors.New("dict keys must be strings")
+		}
+		dict[key] = values[i+1]
+	}
+	return dict, nil
+}
 
 func ifOrDef(eif interface{}, yes interface{}, no interface{}) interface{} {
 	if eif != nil {
@@ -326,6 +342,7 @@ func init() {
 	TemplateFunctions["repeat"] = strings.Repeat
 	TemplateFunctions["hasPrefix"] = strings.HasPrefix
 	TemplateFunctions["hasSuffix"] = strings.HasSuffix
+	TemplateFunctions["dict"] = dict
 	TemplateFunctions["orDef"] = orDef
 	TemplateFunctions["orDefs"] = orDefs
 	TemplateFunctions["ifOrDef"] = ifOrDef
